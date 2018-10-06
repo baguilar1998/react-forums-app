91d9d35ef9a12bd8a358ecfe72b38da6415b6d56
Brian Kenji Aguilar <anjacular@gmail.com>
2018-10-05 20:22:31 -0400

    Redux: Adding redux to the project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// Needed imports for redux
+import { createStore } from 'redux';
+/* A component which will wrap the main application
+ * And it will provide the whole application with the store
+ */
+import {Provider} from 'react-redux';
+import rootReducer from './reducers/rootReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+const initState = {
+    testData:''
+}
+const rootReducer = (state = initState, action) =>{
+    return state;
+}
+
+export default rootReducer;
